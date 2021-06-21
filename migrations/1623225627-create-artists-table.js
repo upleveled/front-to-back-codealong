1a6d93db80b715e22ca4beee77f9a86538d272f9
José Fernando Höwer Barbosa <61600906+Josehower@users.noreply.github.com>
2021-06-21 12:32:09 +0200

    Update 1623225627-create-artists-table.js

--- migrations/1623225627-create-artists-table.js
+++ migrations/1623225627-create-artists-table.js
@@ -1,6 +1,14 @@
+// ⚠️⚠️⚠️ 
+// Before begin this step make sure you have:
+// 
+// - PostgreSQL database configured and ready tu use.
+// - Credentials to the database saved in a .env file.
+// ⚠️⚠️⚠️ 
+//
 // --------------- Step 2 --------------
 //
 // In this step we are going to automate the process of create the schema and fill the database with our data.
+//
 //
 // 2.1 Please create the automation for table artists of our schema
 
