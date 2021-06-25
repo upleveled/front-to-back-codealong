33eb074d0b53b2dbdf80b1a2ed40941986a1b9cc
joshower <josehowerupn@gmail.com>
2021-06-25 10:25:49 +0200

    Uodate code to fix heroku deploy

--- migrations/0001-test-database.js
+++ migrations/0001-test-database.js
@@ -1,7 +1,10 @@
 exports.up = async (sql) => {
   const { current_database } = (await sql`SELECT current_database()`)[0];
   console.log('Your current database is ===>', current_database);
-  if (current_database !== 'upleveled_front_to_back') {
+  if (
+    current_database !== 'upleveled_front_to_back' &&
+    process.env.NODE_ENV !== 'production'
+  ) {
     throw Error('⚠️ ⚠️ ⚠️ ⚠️ -------> wrong DATABASE setup ⚠️ ⚠️ ⚠️ ⚠️');
   }
 };
