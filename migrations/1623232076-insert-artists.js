e3db13376b5a8425b0ad53cfc1ca7004c56fd699
Karl Horky <karl.horky@gmail.com>
2021-06-27 15:24:55 +0200

    Add solutions

--- migrations/1623232076-insert-artists.js
+++ migrations/1623232076-insert-artists.js
@@ -4,18 +4,12 @@ const artistList = [
   { id: 3, name: 'Arrizabalaga, Bravo, Laginestra, Martinez' },
 ];
 
-// 2.2 Please automate the filling of the table artists with the data of our artistList array
-
 exports.up = async function up(sql) {
-  // 1. Define a SQL query that INSERT data into table artists from artistList array
+  await sql`
+    INSERT INTO artists ${sql(artistList, 'id', 'name')}
+  `;
 };
 
 exports.down = async function down(sql) {
-  // 2. Define a SQL query that DELETE all data from table artists
+  await sql`DELETE FROM artists`;
 };
-
-// If this step is doing right and your database is properly configured, you can run the next command
-//
-// yarn migrate up
-//
-// After the completion of this tasks please move to file utils/database.js to continue with the next step.
