397ad589052165133bcc628cbdba0ed028faa588
Karl Horky <karl.horky@gmail.com>
2021-06-27 15:24:55 +0200

    Add solutions

--- migrations/1623225627-create-artists-table.js
+++ migrations/1623225627-create-artists-table.js
@@ -1,25 +1,14 @@
-// ⚠️⚠️⚠️ 
-// Before begin this step make sure you have:
-// 
-// - PostgreSQL database configured and ready tu use.
-// - Credentials to the database saved in a .env file.
-// ⚠️⚠️⚠️ 
-//
-// --------------- Step 2 --------------
-//
-// In this step we are going to automate the process of create the schema and fill the database with our data.
-//
-//
-// 2.1 Please create the automation for table artists of our schema
-
 exports.up = async (sql) => {
-  // 1. Define a SQL query that CREATE Table artists that should contain following columns.
-  //    - column id as unique integer primary key
-  //    - column name as varchar 100
+  await sql`
+    CREATE TABLE artists (
+      id integer PRIMARY KEY UNIQUE NOT NULL,
+      name varchar(100) NOT NULL
+    )
+  `;
 };
 
 exports.down = async (sql) => {
-  // 2. Define a SQL query that DROP table artists
+  await sql`
+    DROP TABLE artists
+  `;
 };
-
-// After this tasks are complete move to file 1623232076-insert-artists.js to find step 2.2 instructions
